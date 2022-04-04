5c671edb1f3725582a87361e2d58c9aeb152f23f
Ondrej Machala <ondra097@seznam.cz>
2022-04-04 16:05:44 +0200

    Add initial menu screen

---
+++ app/src/main/java/cz/muni/fi/circularworkout/ui/menu/MenuFragment.kt
@@ -0,0 +1,19 @@
+package cz.muni.fi.circularworkout.ui.menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cz.muni.fi.circularworkout.R
+
+class MenuFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+}
