91154301856768af480a83eaa26cfb6b1447365c
Ondrej Machala <ondra097@seznam.cz>
2022-04-29 15:05:52 +0200

    Workout detail programmatic creation of views

--- app/src/main/java/cz/muni/fi/circularworkout/ui/workout/detail/WorkoutDetailFragment.kt
+++ app/src/main/java/cz/muni/fi/circularworkout/ui/workout/detail/WorkoutDetailFragment.kt
@@ -1,14 +1,20 @@
 package cz.muni.fi.circularworkout.ui.workout.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.TypedValue.COMPLEX_UNIT_SP
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import cz.muni.fi.circularworkout.R
 import cz.muni.fi.circularworkout.data.WorkoutDetail
 import cz.muni.fi.circularworkout.databinding.FragmentWorkoutDetailBinding
+
 
 class WorkoutDetailFragment : Fragment() {
 
@@ -38,13 +44,28 @@ class WorkoutDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val workout = getMockedInstance(WorkoutDetailFragmentArgs.fromBundle(requireArguments()).name)
         binding.toolbar.title = workout.name
-        binding.exercise1TextView.text = workout.exerciseNames[0]
-        binding.exercise2TextView.text = workout.exerciseNames[1]
-        binding.exercise3TextView.text = workout.exerciseNames[2]
-        binding.exercise4TextView.text = workout.exerciseNames[3]
+        buildExerciseLayout(workout.exerciseNames)
         binding.roundsTextView.text = workout.rounds.toString()
         binding.exerciseTimeTextView.text = workout.exerciseTime.toString()
         binding.restTimeTextView.text = workout.restTime.toString()
+    }
+
+    private fun buildExerciseLayout(exerciseNames: List<String>) =
+        exerciseNames.forEachIndexed { i, exercise ->
+        val txtView = TextView(requireContext())
+        txtView.text = exercise
+        txtView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        txtView.setTextSize(COMPLEX_UNIT_SP, 18F)
+        binding.exercisesLayout.addView(txtView)
+        if (i != exerciseNames.lastIndex) {
+            val image = ImageView(requireContext())
+            image.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                R.drawable.ic_arrow_down_long_solid))
+            binding.exercisesLayout.addView(image)
+        }
     }
 
 }
