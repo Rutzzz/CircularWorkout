ee9ea65c6ef385eb0335722d31e8d5aef5a41665
Alexandra Zemekova <493264@mail.muni.cz>
2022-05-09 21:22:59 +0200

    fragment

--- app/src/main/java/cz/muni/fi/circularworkout/database/CWDatabase.kt
+++ app/src/main/java/cz/muni/fi/circularworkout/database/CWDatabase.kt
@@ -1,14 +1,14 @@
 package cz.muni.fi.circularworkout.database
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import androidx.room.*
 
 @Database(
     entities = [WorkoutEntity::class, WorkoutHistoryEntity::class],
-    version = 1
+    version = 2 ,
+    autoMigrations = [
+            AutoMigration (from = 1, to = 2)],
+    exportSchema = false
 )
 @TypeConverters(Convertets::class)
 abstract class CWDatabase : RoomDatabase() {
